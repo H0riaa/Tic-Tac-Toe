446e3991bd3ba90de1c44f7a88e33239f7ff6278
Horia <noua.aprilie@yahoo.com>
2024-02-21 23:27:05 +0200

    fixed DRAW not working

--- main.cpp
+++ main.cpp
@@ -6,6 +6,7 @@ using namespace std;
 int check();
 void board();
 void color(unsigned int);
+int i;
 
 
 char s[10] = {'o', '1', '2', '3', '4', '5', '6', '7', '8', '9'};   
@@ -45,10 +46,10 @@ int check()
         return 1;
     }
     else(s[1] != '1' && s[2] != '2' && s[3] != '3' &&
-             s[4] != '4' && s[5] != '5' && s[6] != '6' &&
-             s[7] != '7' && s[8] != '8' && s[9] != '9');
+        s[4] != '4' && s[5] != '5' && s[6] != '6' &&
+        s[7] != '7' && s[8] != '8' && s[9] != '9' && i == 0 );
     {
-        return 0;
+        return 2;
     }
     
 }
@@ -204,13 +205,18 @@ void color(unsigned int id)
 
 int main()
 {
- int player = 1;
+    int player = 1;
     int i, choice;
     int mark;
     bool draw = false;
-    do
+     do
     {
         board();
+        if(draw)
+        {
+            cout << "DRAW\n";
+            break;
+        }
         
         cout << "Player " << player << "'s move: \n";
         cin >> choice;
@@ -227,43 +233,50 @@ int main()
         if (choice >= 1 && choice <= 9 && s[choice] == '0' + choice)
         {
             s[choice] = mark;
+            i = check();
+            if(i == 1)
+            {
+                draw = false;
+            }
         }
         else
         {
             cout << "INVALID MOVE!!!!\n";
             
         }
-
-        i = check();
-        
-
-       if(player == 1)
-       {
-        player = 2;
-       }
-       else
-       {
-        player = 1;
-       }
-
-    } while (i == 0);
-
-    board();
-    if (i == 1)
-    {
         if(player == 1)
         {
-            cout << "PLAYER 2 WINS\n";
+            player = 2;
         }
         else
         {
-            cout << "PLAYER 1 WINS\n";
+            player = 1;
         }
-    }
-    else
-    {
-        cout << "DRAW\n";
-    }
+        if(i == 1)
+        {
+            board();
+            if(player == 1)
+            {
+                cout << "PLAY 2 WINS\n";
+            }
+            else
+            {
+                cout << "PLAYER 1 WINS\n";
+            }
+            break;
+        }
 
+        draw = true;
+        for(int j = 1; j <= 9; j++)
+        {
+            if(s[j] == '0' + j)
+            {
+                draw = false;
+                break;
+            }
+        }
+    } 
+    while (true);
 
+    return 0;
 }
